a1fdd114283c5409f246e5b2a0e75d6aeb871bd2
fzapp <fzapp@smail.uni-loeln.de>
2025-11-26 13:15:31 +0100

    aenderung_Einfuehrung_Aufgabenblatt6_HausAufg1_hatDasErsteMalNichtFunktioniert

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -7,4 +7,16 @@ public class H1_main {
 
 	}
 
+	
+	public static boolean isMirrorArray(int[] a, int [] b) {
+		if(a.length != b.length) {
+			return false;
+		}
+		for (int i=0; i<a.length; i++) {
+			if(a[i] != b[b.length -i -1]) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
