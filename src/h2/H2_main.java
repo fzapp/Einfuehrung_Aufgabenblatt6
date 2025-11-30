1178acf94b983e29606d7b23232d1d9e094c3845
fzapp <fzapp@smail.uni-loeln.de>
2025-11-30 21:35:08 +0100

    aenderung_Einfuehrung_Aufgabenblatt6_HausAufg2_Versuch3

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -6,5 +6,17 @@ public class H2_main {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public static boolean compareArrays(int[] a, int[] b) {
+		if(a.length != b.length) {
+			return false;
+		}
+		for(int i=0; i<a.length; i++) {
+			if(a[i] != b[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
